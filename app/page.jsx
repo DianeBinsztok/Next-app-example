5be6d2c6b210e0c6fd35f872279df7c3f0691ca5
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-17 17:34:05 +0200

    style: subheading Tailwind classes

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">My NextJS app</h1>
+        <h1 className="head_text text-center">
+            My NextJS app
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center"> Just following a tutorial to learn NextJS ¯\_(ツ)_/¯</span>
+        </h1>
+
     </section>
   )
 }
 /*
 text-center => classe utilitaire de Tailwind => text-align:center;
+max-md:hidden => Tailwind => caché sur les écrans larges
 head_text => dans globals.css => @apply mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl;
+orange_gradient => globals.css => @apply bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent;
 */
 export default Home;
