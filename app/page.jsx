2f53e9a5b7f574c31120b09ceb4088c152218078
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-17 17:38:35 +0200

    content: app description

--- app/page.jsx
+++ app/page.jsx
@@ -4,16 +4,11 @@ const Home = () => {
         <h1 className="head_text text-center">
             My NextJS app
             <br className="max-md:hidden"/>
-            <span className="orange_gradient text-center"> Just following a tutorial to learn NextJS ¯\_(ツ)_/¯</span>
+            <span className="orange_gradient text-center"> A random app with NextJS</span>
         </h1>
+        <p className="desc text-center">Just following a tutorial to learn NextJS and Tailwind ¯\_(ツ)_/¯</p>
 
     </section>
   )
 }
-/*
-text-center => classe utilitaire de Tailwind => text-align:center;
-max-md:hidden => Tailwind => caché sur les écrans larges
-head_text => dans globals.css => @apply mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl;
-orange_gradient => globals.css => @apply bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent;
-*/
 export default Home;
