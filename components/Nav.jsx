7c0735429922bb40efd638f338220df80202fa1a
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-18 14:25:15 +0200

    feat: create post button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-
-//Composants React intégrés:
 import Link from 'next/link';
-import Image from 'next/image'; //Optimise automatiquement les images
-
-
+import Image from 'next/image';
 
 const Nav = () => {
+
+  const userLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href='/' className='flex gap-2 flex-center'>
@@ -16,7 +14,23 @@ const Nav = () => {
             width={30}
             height={30}
             className='object-contain'/>
+            <p className='logo_text'>Home</p>
         </Link>
+
+         {/* Pour créer un nouveau post */}
+         <div className='sm:flex hidden'>
+
+          {/* Condition ternaire: 
+          si userLoggedIn => afficher le lien 
+          sinon => afficher les balises vides<></>*/}
+          {userLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href="/create-post" className='black_btn'>
+            Créer un post
+            </Link>
+          </div>
+          ):(<></>)}
+         </div>
     </nav>
   )
 }
